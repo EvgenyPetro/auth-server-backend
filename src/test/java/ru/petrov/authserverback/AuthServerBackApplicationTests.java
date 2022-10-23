c7a23b68cf7f9c3d5a8c781849ad135bfaf99a12
Evgeny Petrov <petroves88@mail.ru>
2022-10-23 16:46:53 +0300

    init commit

---
+++ src/test/java/ru/petrov/authserverback/AuthServerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.petrov.authserverback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
